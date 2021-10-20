8b6a77d388098c46091094529d3a2296faec31cf
roksrohit <92860559+roksrohit@users.noreply.github.com>
2021-10-20 13:50:42 +0200

    Add files via upload

--- rohit.c
+++ rohit.c
@@ -3,7 +3,14 @@ int main()
 {
 int x,num;
 scanf("%d",&x);
-num = (x<<5)-x;
+num= x>>5;
 printf("%d",num);
+num= x<<5;
+printf("%d",num);
+num= (x<<5)-x;
+printf("%d",num);
+num= (x<<5)+x;
+
+printf("%d",~x);
 return 0;
 }
