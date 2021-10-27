5bacc9ed4255c7da9f7b739bb60f74641897cf21
roksrohit <92860559+roksrohit@users.noreply.github.com>
2021-10-27 17:13:36 +0530

    Add files via upload

--- rohit.c
+++ rohit.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-int x,num;
-scanf("%d",&x);
-num= x>>5;
-printf("%d",num);
-num= x<<5;
-printf("%d",num);
-num= (x<<5)-x;
-printf("%d",num);
-num= (x<<5)+x;
+int n,a,b,c,reverse;
+printf("enter the numbers");
+scanf("%d",&n);
+a=(n/100);
+b=(n%100) / 10;
+c=(n%10);   
+   
+ reverse = c*100+b*10+a*1;
 
-printf("%d",~x);
+
+printf("the reverse of the number is %d",reverse);
 return 0;
 }
